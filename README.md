1174f78666e0b6a21383f7e8ddbd163a7394d80a
Asset Malik <asetmalik@gmail.com>
2020-09-14 16:18:21 +0600

    change to step freq

--- README.md
+++ README.md
@@ -11,3 +11,4 @@ This repository was created on the 3rd lab and contains packag assigned to it:
 # Packages
 ## moveit
 it is a simple package that has *movejoint* executable that executes squarewave to record the step response with amplitude of 1 rad
+
--- src/moveit/src/movejoint.cpp
+++ src/moveit/src/movejoint.cpp
@@ -6,7 +6,7 @@ int main(int argc, char **argv){
     ros::init(argc, argv, "rotate");
     ros::NodeHandle nh;
     ros::Publisher pub3 = nh.advertise<std_msgs::Float64>("/robot/joint1_position_controller/command", 100);
-    ros::Rate loop_rate(0.1);
+    ros::Rate loop_rate(0.3);
     ros::Time startTime = ros::Time::now();
 
     bool initP = false;
